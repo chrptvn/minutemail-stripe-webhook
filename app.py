6a24b0ffdf1fc43b6baa413b8061a46000811fc7
cacpot <christian.potvin@vanderlande.com>
2025-08-08 07:47:20 -0400

    wip

--- app.py
+++ app.py
@@ -16,32 +16,31 @@ app = FastAPI()
 
 @app.post("/v1/stripe_webhook", status_code=status.HTTP_200_OK)
 def keycloak_webhook(data: Dict):
-    customer_id = data['data']['object']['customer']
+    payload = {
+        "subscription_id": data['data']['object']['id'],
+        "customer_id": data['data']['object']['customer'],
+        "product_id": data['data']['object']['items']['data'][0]['plan']['product'],
+        "subscription_item_id": data['data']['object']['items']['data'][0]['id'],
+    }
+
     if data['type'] == 'customer.subscription.created':
-        payload = {
-          "subscription_id": data['data']['object']['id'],
-          "customer_id": customer_id,
-          "product_id": data['data']['object']['items']['data'][0]['plan']['product'],
-          "subscription_item_id": data['data']['object']['items']['data'][0]['id'],
-        }
-        logging.info(f"Activating membership {payload}")
         requests.post("http://minutemail-subscription-api.minutemail.svc.cluster.local:8080/v1/membership/activate",
                       json=payload,
                       headers={"Content-Type": "application/json"})
     elif data['type'] == 'customer.subscription.deleted':
-        requests.post("http://minutemail-subscription-api.minutemail.svc.cluster.local:8080/v1/membership/deactivate",
-                      json={"customer_id": customer_id, "product_id": data['data']['object']['items']['data'][0]['price']['product']},
+        requests.post("http://minutemail-subscription-api.minutemail.svc.cluster.local:8080/v1/membership/cancel_premium",
+                      json=payload,
                       headers={"Content-Type": "application/json"})
-        logging.info(f"Deactivated membership for customer_id: {customer_id}")
-    elif data['type'] == 'customer.subscription.updated':
-        payload = {
-          "subscription_id": data['data']['object']['id'],
-          "customer_id": customer_id,
-          "product_id": data['data']['object']['items']['data'][0]['plan']['product'],
-          "subscription_item_id": data['data']['object']['items']['data'][0]['id'],
-        }
-        logging.info(f"Updating membership {payload}")
+    elif data['type'] == 'customer.subscription.paused':
+        requests.post("http://minutemail-subscription-api.minutemail.svc.cluster.local:8080/v1/membership/cancel_premium",
+                      json=payload,
+                      headers={"Content-Type": "application/json"})
+    elif data['type'] == 'customer.subscription.resumed':
         requests.post("http://minutemail-subscription-api.minutemail.svc.cluster.local:8080/v1/membership/activate",
+                      json=payload,
+                      headers={"Content-Type": "application/json"})
+    elif data['type'] == 'customer.subscription.updated':
+        requests.post("http://minutemail-subscription-api.minutemail.svc.cluster.local:8080/v1/membership/update",
                       json=payload,
                       headers={"Content-Type": "application/json"})
     return {"status": "success", "message": "Webhook processed successfully"}
